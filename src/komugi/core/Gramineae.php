da6e44bc5b11e1de5e5286fb8b80095dea9a09b4
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2023-11-21 00:56:30 +0900

    discordにおくるやつ

--- src/komugi/core/Gramineae.php
+++ src/komugi/core/Gramineae.php
@@ -5,6 +5,11 @@ namespace komugi\core;
 use pocketmine\plugin\PluginBase;
 use komugi\core\command\CommandMap;
 use komugi\core\event\EventManager;
+use pocketmine\utils\Config;
+
+use bbo51dog\pmdiscord\connection\Webhook;
+use bbo51dog\pmdiscord\element\Content;
+
 
 class Gramineae extends PluginBase
 {
@@ -13,5 +18,23 @@ class Gramineae extends PluginBase
         date_default_timezone_set('Asia/Tokyo');
         EventManager::registerEvents($this);
         CommandMap::registerCommands($this);
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, [
+            'webhooktoken' => 'https://discordapp.com/api/webhooks/00000/xxxxx'
+        ]);
+        $webhook = Webhook::create($this->config->get('webhooktoken'));
+        $content = new Content();
+        $content->setText("起動しました");
+        $webhook->add($content);
+        $webhook->send();
     }
+
+    public function onDisable(): void
+    {
+        $webhook = Webhook::create($this->config->get('webhooktoken'));
+        $content = new Content();
+        $content->setText("停止");
+        $webhook->add($content);
+        $webhook->send();
+    }
+
 }
